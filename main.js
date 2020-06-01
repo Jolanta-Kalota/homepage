8c9008549a768977fd6288e0a6e53f613676f451
Kamil Kalota <kamilkalota@gmail.com>
2020-06-01 19:39:14 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const name = 'Jola';
+const age = 21;
+
+alert('Cześć! Jestem Jola :)');
+console.log('Witaj w konsoli!');
+console.log(name);
+console.log(age);
+console.log(`Hejka nazywam się ${name} i mam ${age} lat.`);
