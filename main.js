a73057c3e4dd8335597c05f14feb56b6a3b6534a
Jolanta Kalota <j.wasiak360@gmail.com>
2020-07-06 17:09:11 +0200

    backgrounds

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ console.log("Witaj w konsoli!");
 console.log(name);
 console.log(age);
 console.log(`Hejka nazywam się ${name} i mam ${age} lat.`);
+
 const JS_paragraph = document.querySelector(`.week-note__description--JS`)  
 console.log(JS_paragraph) 
-JS_paragraph.innerHTML = `nic`  
+JS_paragraph.innerHTML = `nic`
+
+function helloWorld(age, name){
+    console.log(
+        `Hejka nazywam się ${name} i mam ${age} lat.`
+        );
+}
+
+helloWorld(age, name)
